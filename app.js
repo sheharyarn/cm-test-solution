451d7034ac4068c921e396e27e7d8dbf7a39e78f
Sheharyar Naseer <sheharyar92@live.com>
2015-03-12 12:45:57 +0500

    Cleaning up

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express      = require('express');
+var path         = require('path');
+var logger       = require('morgan');
+var bodyParser   = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var app          = express();
 
-var app = express();
 
-// view engine setup
+// View Engine Setup
+// -----------------
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
+
+// App Routes
+// ----------
+
+app.use('/I/want/title', require('./routes/i_want_title'));
+
+
+
+// Error Handlers
+// --------------
+
+// Catch 404s and forward to Error Handlers
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
+// Development Error Handler
+// Prints Stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -46,8 +49,8 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production Error Handler
+// No Stacktrace
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -55,6 +58,7 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
 
 
 module.exports = app;
